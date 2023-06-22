f98570f31d43b4a38b4b25c89e9675b136a28ffc
Antonio Riccelli <anton.riccelli@gmail.com>
2023-06-22 17:16:17 +0100

    adjust indentation and move patch

--- tests/test_update_visitor_count.py
+++ tests/test_update_visitor_count.py
@@ -32,49 +32,48 @@ class TestUpdateVisitorCount(unittest.TestCase):
         os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
         os.environ['DYNAMODB_TABLE_NAME'] = self.table_name
 
-    def test_lambda_handler(self):        
+    def test_lambda_handler(self):
         # Mock DynamoDB Table resource
-        table_mock = self.dynamodb.Table(self.table_name)
+        with patch('boto3.resource') as resource_mock:
+            dynamodb_mock = resource_mock.return_value
+            table_mock = dynamodb_mock.Table.return_value
 
-        # Set up initial item in table
-        table_mock.put_item(Item={'project': 'cloud-resume-challenge', 'visitors': 0})
+            # Set up initial item in table
+            table_mock.put_item(Item={'project': 'cloud-resume-challenge', 'visitors': 0})
 
-        # Patch the boto3.resource and table.get_item calls in the lambda function
-        with patch('boto3.resource') as resource_mock, patch.object(table_mock, 'get_item') as get_item_mock, patch.object(table_mock, 'update_item') as update_item_mock:
-            # Set return values of mocks
-            resource_mock.return_value = self.dynamodb
-            get_item_mock.return_value = {
-                'Item': {
-                    'visitors' : 0 # Mock current value of visitors
+            # Patch the table.get_item and table.update_item calls in the lambda function
+            with patch.object(table_mock, 'get_item') as get_item_mock, patch.object(table_mock, 'update_item') as update_item_mock:
+                # Set return values of mocks
+                get_item_mock.return_value = {
+                    'Item': {
+                        'visitors': 0  # Mock current value of visitors
+                    }
                 }
-            }
 
-            # Call lambda handler with sample event
-            event = {}
-            response = lambda_handler(event, None)
+                # Call lambda handler with sample event
+                event = {}
+                response = lambda_handler(event, None)
 
-            # Assert that the expected DynamoDB methods were called
-            get_item_mock.assert_called_with(
-            TableName=self.table_name,
-            Key={'project': 'cloud-resume-challenge'},
-            ProjectionExpression='visitors'
-            )
-            update_item_mock.assert_called_with(
-            TableName=self.table_name,
-            Key={'project': 'cloud-resume-challenge'},
-            UpdateExpression='SET visitors = :start + :incr',
-            ExpressionAttributeValues={':start': 5, ':incr': 1},
-            ReturnValues='UPDATED_NEW'
-            )
+                # Assert that the expected DynamoDB methods were called
+                get_item_mock.assert_called_with(
+                    Key={'project': 'cloud-resume-challenge'},
+                    ProjectionExpression='visitors'
+                )
+                update_item_mock.assert_called_with(
+                    Key={'project': 'cloud-resume-challenge'},
+                    UpdateExpression='SET visitors = :start + :incr',
+                    ExpressionAttributeValues={':start': 5, ':incr': 1},
+                    ReturnValues='UPDATED_NEW'
+                )
 
-        # Assert response from the lambda function
+            # Assert response from the lambda function
             expected_response = {
-            'statusCode': 200,
-            'body': 'Visitor counter updated. Current count: 1',
-            'count': '1'
+                'statusCode': 200,
+                'body': 'Visitor counter updated. Current count: 1',
+                'count': '1'
             }
             self.assertEqual(response, expected_response)
-    
+
     def tearDown(self):
         # Stop mock dynamodb
         self.table.delete()
